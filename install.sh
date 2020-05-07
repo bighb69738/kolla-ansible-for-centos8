cb181d0001e179b57fc2a088088afa6cc9686ef6
bighb69738 <38970513+bighb69738@users.noreply.github.com>
2020-05-07 10:13:09 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum install python3-devel libffi-devel gcc openssl-devel libselinux-python3 -y
+update-alternatives --install /usr/bin/unversioned-python python /usr/bin/python3.6 1
+update-alternatives --set python /usr/bin/python3
+pip3 install ansible==2.9.7
+yum install git -y
+git clone https://github.com/openstack/kolla -b stable/train
+git clone https://github.com/openstack/kolla-ansible -b stable/train
+pip3 install ./kolla
+pip3 install ./kolla-ansible
+mkdir -p /etc/kolla
+chown $USER:$USER /etc/kolla
+cp -r kolla-ansible/etc/kolla/* /etc/kolla
+cp kolla-ansible/ansible/inventory/* .
+yum install epel-release -y
+yum repolist
+yum install ansible -y
