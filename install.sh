467574f14a8f5a16019cd7fa3d4606743a62625a
bighb69738 <38970513+bighb69738@users.noreply.github.com>
2020-05-07 14:10:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ yum install python3-devel libffi-devel gcc openssl-devel libselinux-python3 -y
 update-alternatives --install /usr/bin/unversioned-python python /usr/bin/python3.6 1
 update-alternatives --set python /usr/bin/python3
 pip3 install ansible==2.9.7
-yum install git -y
 git clone https://github.com/openstack/kolla -b stable/train
 git clone https://github.com/openstack/kolla-ansible -b stable/train
 pip3 install ./kolla
